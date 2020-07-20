cc5389b0e412e1905512b702e3d43c6402f78448
gruzzly-bear <facebawx@gmail.com>
2020-07-20 02:28:18 -0400

    update

--- JS/Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-
-var duck = "quack", dog = "woof", cat = "meow"
-var duck = duck.fontcolor("yellow");
-var dog = dog.fontcolor("brown");
-var cat = cat.fontcolor("orange");
-
-document.write(cat);
--- JavaScript Projects/Javascript/JavaScript_3.js
+++
@@ -1,4 +0,0 @@
-function displayType(character) {
-    var characterType = character.getAttribute("data-character-type");
-    alert(characterType + " is a breed of " + character.innerHTML);
-}
